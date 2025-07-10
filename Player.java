5cbccb570c1b7c4e2475662dde886da06cafea91
consumer-15 <irikyo.15@gmail.com>
2025-07-10 15:11:02 +0900

    継続処理等のバグ修正/PlayerStateをpublicに

--- Player.java
+++ Player.java
@@ -1,16 +1,7 @@
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-enum PlayerState {
-    WAITING, // 初期状態
-    READY, // 「OK」を送信した状態
-    BET, // ベットを完了した状態
-    PLAYING, // ヒット/スタンドを選択できる状態
-    STAND, // スタンドを選択した or バーストした状態
-    LOGOUT, // 継続しないことを選択した状態
-    SPECTATOR,// 観戦状態
-    // ここに他の状態を追加していく
-}
+
 
 public class Player {
     private String name;
